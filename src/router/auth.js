0f93fc0a84e1b381dc115ef0fffa05a043b9af8a
Yannick <gosseyannick553@gmail.com>
2024-12-17 17:01:15 +0000

    feat: login users and generate jwt signed with users id and a jwt secret from the .env file

--- src/router/auth.js
+++ src/router/auth.js
@@ -6,5 +6,12 @@ const { validationSchemas } = require("../utils/validations/user");
 const router = Router();
 
 router.post("/signup", checkSchema(validationSchemas), authController.signup);
-
+router.post(
+  "/login",
+  [
+    body("email").notEmpty().isEmail().withMessage("The email is not valid"),
+    body("password").notEmpty().withMessage("The password is not defined"),
+  ],
+  authController.login
+);
 module.exports = router;
