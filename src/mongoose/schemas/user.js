2de279bc855dd320fdacd167baa0892ca38a0e0e
Yannick <gosseyannick553@gmail.com>
2024-12-18 10:19:47 +0000

    feat(db): take users role into account when creating them

--- src/mongoose/schemas/user.js
+++ src/mongoose/schemas/user.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.String,
     min: 8,
   },
+  role: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
